0369bffc207245649d04a2b1ac0f88a66210e321
prasannakanigiri <34467010+prasannakanigiri@users.noreply.github.com>
2017-12-12 10:28:32 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a[50],i;
+printf("enter the elements into an array");
+scanf("%d",&a[i]);
+for(i=0;i<5;i++)
+   {
+    scanf("%d",&a[i]);
+    }
+for(i=0;i<5;i++)
+   {
+    printf("%d",a[i]);
+   }
+}
